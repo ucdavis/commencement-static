fce60e99b1079fe3fc359ac95554aab5b89278be
Cal Doval <cydoval@mac.com>
2016-12-20 16:50:34 -0800

    first push

--- _scripts/phantom_tests.js
+++
@@ -1,9 +0,0 @@
-casper.test.begin('Home page', 1, function suite(test) {
-  casper.start("./_site/index.html", function() {
-    test.assertTitle("Home | Feeding Texas", "Feeding Texas homepage title is the one expected");
-  });
-
-  casper.run(function() {
-    test.done();
-  });
-});
